ab45201d5700b34388b2289ba4fd378fc04ff8df
Zennyth <matsfox@orange.fr>
2021-12-16 20:59:47 +0100

    init repo

---
+++ src/main/java/com/example/manager/Sensor.java
@@ -0,0 +1,14 @@
+package com.example.manager;
+
+import java.util.Date;
+
+public class Sensor {
+  public String id;
+  public float latitude;
+  public float longitude;
+  public float intensity;
+  public float radius;
+
+  public Date createdAt;
+  public Date updatedAt;
+}
